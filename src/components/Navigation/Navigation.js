6e16000e299ad281a4844edeac779fe6407808f5
joesphchang <joesphschang@gmail.com>
2023-02-22 20:31:13 -0600

    navigation menu done

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
-function Navigation() {
+const Navigation = () => {
+const [click, setClick] = useState(false);
+
+const handleClick = () => setClick(!click);
+
   return (
-    <div>Navigation</div>
-  )
+		<div className='navbar'>
+			<div className='logo'>
+				<h1>insync.</h1>
+			</div>
+			<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+				<Link className='nav-item'>
+					<a href='/'>Home</a>
+				</Link>
+				<Link className='nav-item'>
+					<a href='/'>About</a>
+				</Link>
+				<Link className='nav-item'>
+					<a href='/'>FAQ</a>
+				</Link>
+				<Link className='nav-item'>
+					<a href='/'>Contact</a>
+				</Link>
+			</ul>
+			<div className='hamburger' onClick={handleClick}>
+				{click ? (
+					<FaTimes size={30}  />
+				) : (
+					<FaBars size={30} />
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Navigation
